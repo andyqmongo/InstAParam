d4da46016ea366991a77d0edf139ff62bed6e195
andyqmongo <andyqmongo@gapp.nthu.edu.tw>
2021-05-28 19:26:23 +0800

    test.py

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import torch.optim as optim
 import numpy as np
 import tqdm 
 import torch.backends.cudnn as cudnn
+import pandas as pd
 cudnn.benchmark = True
 torch.set_num_threads(1)
 
@@ -28,14 +29,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--model', default='InstAParam-single', choices=['InstAParam', 'InstAParam-single'])
 parser.add_argument('--dset_name', default=None, required=True, choices=['C10', 'C100', 'Tiny', 'Fuzzy-C10'])
 parser.add_argument('--data_dir', default='./data/')
-parser.add_argument('--load_dir', required=True, default=None)
+parser.add_argument('--load_dir', default=None)
 parser.add_argument('--batch_size', type=int, default=250)
 parser.add_argument('--mu', default=0.5, type=float, help='threshold for picking the block')
 parser.add_argument('--norm_type', type=str, default='GroupNorm', choices=['GroupNorm', 'BatchNorm'])
-
+parser.add_argument('--loop_through_dir', default=None)
 args = parser.parse_args()
 
-writer = SummaryWriter(os.path.join(args.load_dir, 'log'))
 
 if args.dset_name == 'C10' or args.dset_name == 'Fuzzy-C10':
     task_length = 2
@@ -49,10 +49,10 @@ elif args.dset_name == 'Tiny':
 
 
 
-def test(testloader, test_task, agent, meta_graph):
+def inference(testloader, test_task, agent, meta_graph):
 
     matches_te = []
-    for batch_idx, (inputs, targets) in tqdm.tqdm(enumerate(testloader), total=len(testloader)):
+    for batch_idx, (inputs, targets) in enumerate(testloader):
         
         with torch.no_grad():
             inputs, targets = Variable(inputs).cuda(), Variable(targets).cuda()
@@ -72,7 +72,8 @@ def test(testloader, test_task, agent, meta_graph):
         mask = [0.] * (task_length * num_tasks)
         for sub in curSubclass:
             mask[sub] = 1.0
-        preds = preds[:, curSubclass]
+        #preds = preds[:, curSubclass]
+        preds = preds * Variable(torch.tensor(mask)).cuda(non_blocking=True)
 
         _, pred_idx = preds.max(1)
 
@@ -85,33 +86,82 @@ def test(testloader, test_task, agent, meta_graph):
     return testing_acc.cpu().detach().numpy()
 
 
-from dataloader import getDataloaders
-_, testLoaders = getDataloaders(dset_name=args.dset_name, shuffle=True, splits=['test'], 
-        data_root=args.data_dir, batch_size=args.batch_size, num_workers=0, num_tasks=num_tasks, raw=False)
 
-meta_graph, controller = utils.get_model(args.model, args.dset_name, norm_type=args.norm_type)
-meta_graph.cuda()
-controller.cuda()
+def test_one_by_one(testLoaders, model, dset_name, norm_type, load_dir, detailed=False):
+    writer = SummaryWriter(os.path.join(load_dir, 'log'))
 
-accs = np.zeros((num_tasks, num_tasks))
-start_task = 0
-if args.dset_name =='C100' or args.dset_name =='Tiny':
-    start_task = 1
+    meta_graph, controller = utils.get_model(model, dset_name, norm_type=norm_type, detailed=detailed)
+    meta_graph.cuda()
+    controller.cuda()
 
-for t in range(start_task, num_tasks):
-    ckpt_net = torch.load('{}/meta_grpah_{}.t7'.format(args.load_dir, t))
-    meta_graph.load_state_dict(ckpt_net, strict=False)
-    meta_graph.eval().cuda()
-    if args.model == 'InstAParam-single':
-        meta_graph.assign_mask()
+    accs = np.zeros((num_tasks, num_tasks))
+    start_task = 0
+    if dset_name =='C100' or dset_name =='Tiny':
+        start_task = 1
+    if detailed:
+        print("START TASK:{}".format(start_task))
 
-    for tt in range(t):
-        if os.path.exists('{}/controller_{}.t7'.format(args.load_dir, tt)):
-            ckpt = torch.load('{}/controller_{}.t7'.format(args.load_dir, tt))
+    for t in range(start_task, num_tasks):#meta-graph
+        if not os.path.exists('{}/meta_grpah_{}.t7'.format(load_dir, t)):
+            return None
+        try:
+            ckpt_net = torch.load('{}/meta_grpah_{}.t7'.format(load_dir, t))
+        except:
+            return None
+        meta_graph.load_state_dict(ckpt_net, strict=False)
+        meta_graph.eval().cuda()
+        if model == 'InstAParam-single':
+            meta_graph.assign_mask()
+
+        for tt in range(start_task, t+1):#controller
+            if not os.path.exists('{}/controller_{}.t7'.format(load_dir, tt)):
+                return None
+            try:
+                ckpt = torch.load('{}/controller_{}.t7'.format(load_dir, tt))
+            except:
+                return None
             controller.load_state_dict(ckpt)
             controller.eval().cuda()
+            
+            acc = inference(testLoaders[tt], tt, controller, meta_graph)
+            #print("t:{}, tt:{}, acc:{}".format(t, tt, acc))
+            accs[t][tt] = acc
+            
+    if dset_name == 'C10':
+        avg_acc = np.mean(accs[-1])
+    else:
+        avg_acc = np.mean(accs[-1][1:])
+    dir_ = os.path.join(load_dir, 'test_{:.4f}.csv'.format(avg_acc))
+    
+    df = pd.DataFrame(accs)
+    #if saved:
+    df.to_csv(dir_)
+    
+    if detailed:
+        print(accs)
+        print('avg acc:{:.4f}'.format(avg_acc))
 
-        accs[t][tt] = test(testLoaders[tt], tt, controller, meta_graph)
-        
+    return avg_acc
 
-print(accs)
+if __name__ == '__main__':
+    from dataloader import getDataloaders
+    _, testLoaders = getDataloaders(dset_name=args.dset_name, shuffle=True, splits=['test'], 
+            data_root=args.data_dir, batch_size=args.batch_size, num_workers=0, num_tasks=num_tasks, raw=False)
+    if args.loop_through_dir is None:
+        test_one_by_one(testLoaders, args.model, args.dset_name, args.norm_type, args.load_dir, detailed=True)
+    else:
+
+        tables = []
+        listdirs = os.listdir(args.loop_through_dir)
+        for dir in tqdm.tqdm(listdirs):
+            full_dir = os.path.join(args.loop_through_dir, dir)
+            if full_dir.find('GroupNorm') > 0:
+                norm_type = 'GroupNorm'
+            elif full_dir.find('BatchNorm') > 0:
+                norm_type = 'BatchNorm'
+            else:
+                norm_type = args.norm_type
+            avg_acc = test_one_by_one(testLoaders, args.model, args.dset_name, norm_type, full_dir, detailed=False)
+            tables.append([dir, avg_acc])        
+        df_table = pd.DataFrame(tables)
+        df_table.to_csv(os.path.join(args.loop_through_dir, 'result.csv'))
